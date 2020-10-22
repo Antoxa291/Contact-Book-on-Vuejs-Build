75780ff9d858495f00ff4d6916885eb07f65363c
Anton Vorobiov <60668753+Antoxa291@users.noreply.github.com>
2020-10-22 09:48:41 +0300

    init: v. 1.0

---
+++ precache-manifest.f567467ba8eb3dc67e732afe70515e19.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "25ac29c6c91bca513f92",
+    "url": "/css/app.1ab98e92.css"
+  },
+  {
+    "revision": "25ac29c6c91bca513f92",
+    "url": "/js/app.f92b1ae6.js"
+  },
+  {
+    "revision": "689b41a2bfe9e0ca460e",
+    "url": "/css/chunk-1675999c.34b26fa2.css"
+  },
+  {
+    "revision": "689b41a2bfe9e0ca460e",
+    "url": "/js/chunk-1675999c.115372bc.js"
+  },
+  {
+    "revision": "d6b2f05fd2227cc7e965",
+    "url": "/css/chunk-4869f1b6.306d4862.css"
+  },
+  {
+    "revision": "d6b2f05fd2227cc7e965",
+    "url": "/js/chunk-4869f1b6.7bc8c1ff.js"
+  },
+  {
+    "revision": "c05bf91cc2819d800845",
+    "url": "/css/chunk-644ec7c6.d837d910.css"
+  },
+  {
+    "revision": "c05bf91cc2819d800845",
+    "url": "/js/chunk-644ec7c6.91e8de2e.js"
+  },
+  {
+    "revision": "0c56d72d608def3ee1a4",
+    "url": "/css/chunk-vendors.336d00a5.css"
+  },
+  {
+    "revision": "0c56d72d608def3ee1a4",
+    "url": "/js/chunk-vendors.4068efb2.js"
+  },
+  {
+    "revision": "8f667fd4d5a27bac93623ed9e9aa31a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
